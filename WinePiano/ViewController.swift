f537274fbff9fa78317ee2a45c9ff6e5c37d7d90
mizugaki <takeshi.mizugaki@ast-net.co.jp>
2015-06-05 17:25:31 +0900

    Initial Commit

---
+++ WinePiano/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WinePiano
+//
+//  Created by Mizugaki on 2015/06/05.
+//  Copyright (c) 2015年 ast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
